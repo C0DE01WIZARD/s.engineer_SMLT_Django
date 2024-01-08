270a1efed72eaa016387c542e7fc772016544602
Rushan <usmanov-rushan@mail.ru>
2024-01-08 20:07:50 +0300

    adding page equipments.html

--- engineer/systems/urls.py
+++ engineer/systems/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import Equipments
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('equipments/', Equipments.as_view(), name='equipments'),
 ]
--- engineer/systems/views.py
+++ engineer/systems/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from.models import *
+
 
 def main(request):
     return render(request, "main.html")
+
+
+class Equipments(ListView):
+    model = Systems
+    template_name = "equipments.html"
+    context_object_name = 'systems'
+
+
+
